4837544919b95bde9ea8bb67f70507c2eb3854a6
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-07 21:16:42 +0530

    done

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { CartContext } from "../Context/CartContext/CartContextProvider";
 import { useContext } from "react";
 import { Table, TableContainer, Tbody, Th,Td ,Tr, Heading,Button, useDisclosure, AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter} from "@chakra-ui/react";
-// 1. cart page should contain all the cart items that are saved in cart context in the form of a table; ( you can display atleast product name and product price in first two cells of any row). the third cell contains remove from cart button clicking on which shall remove the item from the cart
 
-// 2. Below all the cart items list on table foot; there should be a Total in first cell of row and Total (Total price of all items in cart ) in second cell;
-
-// 3. below the table; there should be a button called `Place Order` clicking on which will open an alert dialog box which asks `Are you sure you want to place this order ?` and two buttons Cancel and Yes; clicking on cancel will close the alert dialog; click on yes will dispatch checkout action which will empty all the cart items basically. cart items are empty after this.
 
 const Cart = () => {
   let {addToCartdata,settotalamount,totalamount,handleRemove,handleCheckOut} =  useContext(CartContext);
